707e8f7465b827edb01fbf30014f99bc4beadb35
Jay Wiggins <jayw@Jays-MacBook.local>
2018-01-17 14:44:44 -0700

    slow down and dim...

--- test_max7219_led_matrix/src/main.cpp
+++ test_max7219_led_matrix/src/main.cpp
@@ -11,39 +11,36 @@
 #include <MAX7219_Dot_Matrix.h>
 const byte chips = 4;
 
-// 12 chips (display modules), hardware SPI with load on D10
-MAX7219_Dot_Matrix display (chips, 10);  // Chips / LOAD
+// 4 chips (display modules), hardware SPI with load on D10
+MAX7219_Dot_Matrix display(chips, 10);  // Chips / LOAD
 
-const char message [] = "Testing 1234567890";
+const char message[] = "Testing 1234567890";
 
-void setup ()
-  {
-  display.begin ();
-  }  // end of setup
+void setup(){
+  display.begin();
+  display.setIntensity(8);
+}  // end of setup
 
 unsigned long lastMoved = 0;
-unsigned long MOVE_INTERVAL = 20;  // mS
+unsigned long MOVE_INTERVAL = 40;  // mS
 int  messageOffset;
 
-void updateDisplay ()
-  {
-  display.sendSmooth (message, messageOffset);
+void updateDisplay() {
+  display.sendSmooth(message, messageOffset);
 
   // next time show one pixel onwards
-  if (messageOffset++ >= (int) (strlen (message) * 8))
+  if(messageOffset++ >= (int)(strlen (message) * 8))
     messageOffset = - chips * 8;
-  }  // end of updateDisplay
+}  // end of updateDisplay
 
-void loop ()
-  {
+void loop() {
 
   // update display if time is up
-  if (millis () - lastMoved >= MOVE_INTERVAL)
-    {
-    updateDisplay ();
-    lastMoved = millis ();
-    }
+  if(millis() - lastMoved >= MOVE_INTERVAL) {
+    updateDisplay();
+    lastMoved = millis();
+  }
 
   // do other stuff here
 
-  }  // end of loop
+}  // end of loop
